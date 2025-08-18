9540e855ea6244413cf098c66afce4a0ed692424
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-08-18 13:00:45 +0200

    Set up prettier and lint the whole project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,16 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
-import {routesGenerator} from 'angular-ui-generator';
-import {HomeView} from './view/home.view';
+import { routesGenerator } from 'angular-ui-generator';
+import { HomeView } from './view/home.view';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routesGenerator(HomeView))
-  ]
+    provideRouter(routesGenerator(HomeView)),
+  ],
 };
