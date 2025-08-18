d115d614f536839ca39c16dd776ac5206b99bfb5
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-08-18 12:57:36 +0200

    Create basic hello world application

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,12 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import {routesGenerator} from 'angular-ui-generator';
+import {HomeView} from './view/home.view';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routesGenerator(HomeView))
   ]
 };
---
+++ src/app/view/home.view.ts
@@ -0,0 +1,17 @@
+import {Component} from '@angular/core';
+import {ParagraphBuilder, RegisterView, UiElement, ViewGenerator} from 'angular-ui-generator';
+
+@Component({
+  selector: 'home-view',
+  template: '<lib-generator-api-view-generator [uiElements]="uiElements"></lib-generator-api-view-generator>',
+  imports: [ViewGenerator]
+})
+@RegisterView({
+  name: 'home',
+  children: []
+})
+export class HomeView {
+  uiElements: UiElement[] = [
+    ParagraphBuilder.build("Hello World!")
+  ];
+}
